155311dafa0b04b0e9969e815125e040ba9021aa
Tamas Toth <tommey@tommey.hu>
2015-08-27 05:45:32 -0400

    Added stub to avoid file read

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-
+var fs = require('fs');
 var sut = require('./index');
 
 describe('String.toUpperCase', function(){
@@ -10,8 +10,12 @@ describe('String.toUpperCase', function(){
 
 describe('index', function(){
 	it('reads the name from the package.json', function(done){
+		this.sandbox.stub(fs, 'readFile', function(path, encoding, callback){
+			return callback(null, '{"name": "stub101"}');
+		});
+
 		sut.read(function(err, name) { 
-			expect(name).to.eql('test101');
+			expect(name).to.eql('stub101');
 			done(err);
 		});
 	});
