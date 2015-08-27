20c63db1c5d3da611fa83f65dde161444e4038f4
Tamas Toth <tommey@tommey.hu>
2015-08-27 05:10:14 -0400

    Added first test for checking the String.toUpperCase function

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,8 @@
+var expect = require('chai').expect;
+
 describe('String.toUpperCase', function(){
 	it('returns uppercased values', function(){
-		
+		expect('apple'.toUpperCase()).to.eql('APPLE');
 	});
 });
+
